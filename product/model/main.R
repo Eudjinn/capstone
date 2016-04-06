503d940d26dbaa84b457d5b0f9b1fd055dbe1f2d
Eudjinn <eugeneios@gmail.com>
2016-04-06 09:32:04 +0300

    minor

--- product/model/main.R
+++ product/model/main.R
@@ -12,9 +12,9 @@ no_cores <- max(1, detectCores() - 1)
 options(mc.cores = no_cores)
 #options(datatable.verbose=TRUE)
 # number of rows from original docs to use
-sample.percent <- 0.01
+sample.percent <- 0.8
 # proportion of training set
-train.percent <- 0.9
+train.percent <- 0.95
 #parallel processing
 parallel = TRUE
 
